b79ac8a298fae8506e9ba52bcb479c27bc2df896
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-03-14 09:42:51 -0400

    work on auth, register works, working on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,59 @@
 class UsersController < ApplicationController
-  def index
-    users = User.all
-    render json: users
-  end
+  
+    def index
+      puts 'called'
+      session[:session_token] = 3
+      render json: [1, 2, 3, 4]
+    end
 
-  def show
-    user = User.find(params[:id])
-    render json: user
-  end
+    def gen_token(user_id)
+      payload = {id: user_id}
+      JWT.encode(payload, Rails.application.secrets.secret_key_base) 
+    end
 
-  def create
-    user = User.create!(user_params)
-    render json: user
-  end
+    def create
+      email = params[:email]
+      password = params[:password]
+      first_name = params[:first_name]
+      last_name = params[:last_name]
 
-  def update
-    user = User.find(params[:id])
-    user.update!(user_params)
-    render json: user
-  end
+      new_user = User.new({
+        password: password,
+        email: email,
+        first_name: first_name,
+        last_name: last_name
+      })
 
-  def destroy
-    user = User.find(params[:id])
-    user.destroy!
-    render plain: "User deleted ... but if that's true, who am I talking to?"
-  end
 
-  private
+      if new_user.valid?
+        new_user.save
+        render json: {token: gen_token(new_user.id)}
+      else
+        render nothing: true, status: 401
+      end
+    end
 
-  def user_params
-    params.require(:users).permit(:first_name, :last_name, :email, :password_digest)
-  end
+    def index
+      @users = User.all
+    end
+
+    def is_logged_in
+      if current_user
+        render json: current_user
+      else 
+        render nothing: true, status: 401
+      end
+    end
+
+    def login
+      username = params[:username]
+      password = params[:password]
+
+      user = User.find_from_credentials username, password
+      if user.nil?
+        render nothing: true, status: 401
+      else 
+        render json: {user: user, token: gen_token(user.id)}
+      end
+    end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :users
+  get 'isLoggedIn', :to => 'users#is_logged_in'
+  post 'users/login', :to => 'users#login'
   resources :jokes
   resources :routines
   resources :routine_with_jokes
-
 end
