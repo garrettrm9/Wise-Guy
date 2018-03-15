0e00134221c1ed4d496638790d5860fad4fa3f14
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-03-15 12:23:51 -0400

    PUT routines and jokes work on front ... MVP is hitgit add .

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   resources :users
   get 'isLoggedIn', :to => 'users#is_logged_in'
   post 'users/login', :to => 'users#login'
-  resources :jokes, :except => ['show', 'post', 'delete']
+  resources :jokes, :except => ['show', 'post', 'delete', 'put']
   get 'users/:id/jokes' => 'jokes#show'
   post 'users/:id/jokes' => 'jokes#create'
   delete 'users/:id/jokes/:id' => 'jokes#destroy'
-  resources :routines, :except => ['show']
+  put 'users/:id/jokes/:id' => 'jokes#update'
+  resources :routines, :except => ['show', 'post', 'delete', 'put']
   get 'users/:id/routines' => 'routines#show'
   post 'users/:id/routines' => 'routines#create'  
   delete 'users/:id/routines/:id' => 'routines#destroy'
+  put 'users/:id/routines/:id' => 'routines#update'
   resources :routine_with_jokes
 end
